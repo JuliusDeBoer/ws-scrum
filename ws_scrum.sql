706c794db4a3cac757ae7b63313edf7230c93c25
Julius de Boer <JuliusDeBoer@users.noreply.github.com>
2023-05-24 13:40:23 +0200

    Add password column

--- ws_scrum.sql
+++ ws_scrum.sql
@@ -31,7 +31,8 @@ CREATE TABLE `admin` (
   `id` int(11) NOT NULL,
   `FirstName` varchar(128) NOT NULL,
   `LastName` varchar(128) NOT NULL,
-  `email` varchar(128) NOT NULL
+  `email` varchar(128) NOT NULL,
+	`password` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
