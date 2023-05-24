2fcb76560fa720cd4cbfb63171f174b9f53771f7
samromer <sam.romer@student.graafschapcollege.nl>
2023-05-24 13:29:26 +0200

    html + css for login page

--- public/index.php
+++ public/index.php
@@ -1,8 +1,28 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login page</title>
+    <link rel="stylesheet" href="login.css">
 </head>
 <body>
-	<h1>Hello, World!</h1>
+    <div class="container">
+        <div class="content">
+            <h1>Login</h1>
+            <div class="login">
+                <form action="#" method="post">
+                    <label for="email">email</label>
+                    <input type="email" name="email" required>
+                    <br>
+                    <label for="password">password</label>
+                    <input type="password" name="password" required>
+                    <br>
+                    <input value="login" type="submit" name="login">
+                </form>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
