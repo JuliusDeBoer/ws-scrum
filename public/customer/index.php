d34a9e19479b78c98626ebebdb4037ca8a997b1d
samromer <sam.romer@student.graafschapcollege.nl>
2023-05-31 13:29:34 +0200

    added customer edit

---
+++ public/customer/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Customer</title>
+  <link rel="stylesheet" href="../../public/style.css">
+</head>
+<body>
+	<div class="container">
+			<div class="content">
+			<form action="#" method="post">
+			<label for="firstname">firstname</label>
+			<input type="text" name="email" required>
+			<label for="lastname">lastname</label>
+			<input type="text" name="lastname" required>
+			<label for="address">address</label>
+			<input type="text" name="address" required>
+			<br>
+			<label for="discription">discription</label>
+			<input type="text" name="discription" required>
+			<label for="status">status</label>
+			<input type="text" name="status" required>
+			<label for="lastAtc">last action</label>
+			<input type="text" name="lastAtc" required>
+			<br>
+			<input value="submit" type="submit" name="submit">
+			</form>
+		</div>
+	</div>
+</body>
+</html>
