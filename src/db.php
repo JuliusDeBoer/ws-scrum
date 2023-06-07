b8a4caaf4c0288d29d3b4a6841e7517ead429fce
JurreSerrarens <jurre.serrarens@student.graafschapcollege.nl>
2023-06-07 13:03:17 +0200

    Added more comments to the functions

--- src/db.php
+++ src/db.php
@@ -6,11 +6,15 @@ class DB
 {
 	public static mysqli $conn;
 
+	//Return the query result of the given query
 	public function query(string $query): mysqli_result
 	{
 		return DB::$conn->query($query);
 	}
 
+	/**Checks if the login value's are valid
+	*  Returns a user class if succesful
+	*  Returns false if unsuccesful**/
 	public function login(string $user, string $password): User | false
 	{
 		$result = $this->query("SELECT * FROM users WHERE Password = \"$password\" AND FirstName = \"$user\" ");
@@ -23,7 +27,10 @@ class DB
 		return false;
 	}
 
-	public function getUserById(int $id): User
+	/**Gets the column with the defined id in the User table within the database
+	*  Returns a customer class if succesful
+	*  Returns false if unsuccesful**/
+	public function getUserById(int $id): User | false
 	{
 		$result = $this->query("SELECT * FROM users WHERE id = ".$id);
 
@@ -35,7 +42,10 @@ class DB
 		return false;
 	}
 
-	public function getCustomerById(int $id): Customer
+	/**Gets the column with the defined id in the cutomer table within the database
+	*  Returns a customer class if succesful
+	*  Returns false if unsuccesful**/
+	public function getCustomerById(int $id): Customer | false
 	{
 		$result = $this->query("SELECT * FROM customers WHERE id = ".$id);
 
@@ -47,22 +57,27 @@ class DB
 		return false;
 	}
 
+	//Add new column to the user table within the database
 	public function addUser(String $fName, String $lName, String $email, String $password)
 	{
 		$this->query("INSERT INTO users (FirstName,LastName,Email,Password) VALUES(\"$fName\",\"$lName\",\"$email\",\"$password\")");
 	}
 
+	//Add new column to the customer table within the database
 	public function addCustomer(String $fName, String $lName, String $desc, String $address, String $status)
 	{
 		$this->query("INSERT INTO customers (FirstName,LastName,Description,Adress,Status) VALUES(\"$fName\",\"$lName\",\"$desc\",\"$address\",\"$status\")");
 	}
 
+	//Update values within the customer table within the database
 	public function updateCustomer(String $fName, String $lName, String $desc, String $address, String $status){
 		$this->query("UPDATE customers SET FirstName = \"$fName\", LastName = \"$lName\", Description = \"$desc\", Adress = \"$address\", Status = \"$status\")");
 	}
 
+	//Update values within the user table within the database
 	public function updateUser(String $fName, String $lName, String $email, String $password){
 		$this->query("UPDATE users SET FirstName = \"$fName\", LastName = \"$lName\", Email = \"$email\", Password = \"$password\"");
 	}
+
 }
 ?>
